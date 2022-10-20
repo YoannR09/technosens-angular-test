930c7f8e05a851a19e331b7adba366918bf6ea8a
Yoann ROCHE <roche_yoann@outlook.fr>
2022-10-20 13:27:41 +0200

    impl const base url

--- src/app/product/product.service.ts
+++ src/app/product/product.service.ts
@@ -11,6 +11,8 @@ export interface Info {
   material: string[]
 }
 
+const BASE_URL = 'https://api-privee'
+
 
 @Injectable()
 export class ProductService {
@@ -22,23 +24,23 @@ export class ProductService {
   async read() {
     // this.infosChanged.next(PRODUCTS_MOCK)
     this.infosChanged.next(
-      await lastValueFrom(this.http.get<Info[]>("https://api-privee/info"))
+      await lastValueFrom(this.http.get<Info[]>(`${BASE_URL}/info`))
     )
   }
 
   async command(info: Info) {
-    await lastValueFrom(this.http.post("https://api-privee/envoyer-commande", info));
+    await lastValueFrom(this.http.post(`${BASE_URL}/envoyer-commande`, info));
   }
 
   async cancel(info: Info) {
-    await lastValueFrom(this.http.post("https://api-privee/cancel-commande", info));
+    await lastValueFrom(this.http.post(`${BASE_URL}/cancel-commande`, info));
   }
 
   async rev(info: Info) {
     if(!this.actionDeVerification()) {
       throw new Error('Verification Failed')
     }
-    await lastValueFrom(this.http.post("https://api-privee/relance", info));
+    await lastValueFrom(this.http.post(`${BASE_URL}/relance`, info));
   }
   
   public actionDeVerification() {
